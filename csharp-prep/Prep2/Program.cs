83fbf7c4c3ed9ea1066be063c658ca52302f181c
Jared Reynolds <icedraggon234@gmail.com>
2023-01-07 23:16:47 -0700

    Completed programs for assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentageText = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageText);
+
+
+        string letterGrade;
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >=60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Letter grade: {letterGrade}");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed. Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed. Keep on trying and you might make it!");
+        }
     }
 }
