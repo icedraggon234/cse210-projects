5f6855693a834c300d74e87be243de39479ef24b
Jared Reynolds <icedraggon234@gmail.com>
2023-02-27 14:45:26 -0700

    Completed Program project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+
+        Square square = new Square("red", 3);
+        Rectangle rectangle = new Rectangle("blue", 5, 7);
+        Circle circle = new Circle("purple", 4);
+
+        List<Shape> shapes = new List<Shape> {square, rectangle, circle};
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
